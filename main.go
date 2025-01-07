45cbce754c442f943ad87231cdc433bc41e80a14
Jeremy Gale <121040268+jgale-nuvoair@users.noreply.github.com>
2025-01-06 17:07:06 -0700

    inline the whole thing

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"path/filepath"
-	"github.com/FriendlyUser/bitrise-step-google-drive-uploader/pkg/utils"
+
+	drive "google.golang.org/api/drive/v3"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"golang.org/x/oauth2/jwt"
 )
+
 
 func main() {
 	// print service_key_path
@@ -55,7 +66,7 @@ func main() {
 	fmt.Println(file)
 
 
-	utils.UploadFile(serviceAccount, os.Getenv("BITRISE_XCODEBUILD_TEST_LOG_PATH"), folderId)
+	UploadFile(serviceAccount, filePath, folderId)
 
 	// upload all apk files
 	//for _, file := range files {
@@ -68,4 +79,61 @@ func main() {
 	//  with a 0 exit code `bitrise` will register your Step as "successful".
 	// Any non zero exit code will be registered as "failed" by `bitrise`.
 	os.Exit(0)
+}
+
+func serviceAccount(credentialFile string) *http.Client {
+	b, err := ioutil.ReadFile(credentialFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var c = struct {
+		Email      string `json:"client_email"`
+		PrivateKey string `json:"private_key"`
+	}{}
+	json.Unmarshal(b, &c)
+	config := &jwt.Config{
+		Email:      c.Email,
+		PrivateKey: []byte(c.PrivateKey),
+		Scopes: []string{
+			drive.DriveScope,
+		},
+		TokenURL: google.JWTTokenURL,
+	}
+	client := config.Client(oauth2.NoContext)
+	return client
+}
+
+func UploadFile(serviceFile string, fileName string, folderId string) {
+	filename := fileName                       // Filename
+	baseMimeType := "application/octet-stream" // MimeType
+	client := serviceAccount(serviceFile)      // Please set the json file of Service account.
+
+	srv, err := drive.New(client)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fileInf, err := file.Stat()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+	// get base name from filename
+	baseName := filepath.Base(filename)
+	f := &drive.File{Name: baseName}
+	if folderId != "" {
+		f.Parents = []string{folderId}
+	}
+	res, err := srv.Files.
+		Create(f).
+		ResumableMedia(context.Background(), file, fileInf.Size(), baseMimeType).
+		ProgressUpdater(func(now, size int64) { fmt.Printf("%d, %d\r", now, size) }).
+		Do()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("Uploaded file %s with id %s\n", filename, res.Id)
 }
